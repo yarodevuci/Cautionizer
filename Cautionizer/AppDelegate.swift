aa017ef32decaf2f1bd829405901d21993ad9f14
Yaroslav Dukal <dukalyar@gmail.com>
2016-04-27 01:15:38 -0700

    Added parse DB frameworks

--- Cautionizer/AppDelegate.swift
+++ Cautionizer/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("3wev7Ce18jDN29jRrAPyE0sDHJnXyo1n3iuLRVu5", clientKey: "bmIjhBzKs7Lx17XDJcwOIjZHoJ5NgQ9tO2xTMZQB")
         return true
     }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
